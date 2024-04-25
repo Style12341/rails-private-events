7535c15eb5158e13b067187bcf2e275496192413
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-25 02:53:15 -0300

    Fix event user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
+  has_many :events, foreign_key: 'creator_id', class_name: 'Event'
 end
