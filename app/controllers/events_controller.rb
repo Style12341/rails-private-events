19b52ae4100cd7ea187aeb614cd32368283679f0
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-25 02:57:04 -0300

    Add new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,29 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:notice] = 'Event created!'
+      redirect_to @event
+    else
+      flash.now[:alert] = 'Failed to create event'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
 end
