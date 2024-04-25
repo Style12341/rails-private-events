2f040fe37ef43a541fa910b36d3b206636ef31f0
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-25 02:28:14 -0300

    Add event controller & devise parameter santization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
-require "application_responder"
+require 'application_responder'
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
 end
