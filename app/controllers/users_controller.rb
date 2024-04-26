4c5f520856c2741090ac949a5e9cde7bc4af6448
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-26 00:54:06 -0300

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show]
   def show
     @user = User.find(params[:id])
+    if @user != current_user
+      flash[:alert] = 'You can only view your own profile'
+      redirect_to root_path
+    end
     @events = @user.events
     @attended_events = @user.attended
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :event_attendances, source: :attendee
+
+  scope :past, -> { where('date < ?', Time.now).order('date DESC') }
+  scope :upcoming, -> { where('date >= ?', Time.now).order('date ASC') }
+
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :creator_id, presence: true
 end
